1616a05a24267bd1f6995cea296126ddf5bd6114
ulempicka <urszula.lempicka@gmail.com>
2020-08-18 18:39:20 +0200

    Commit6 - updated text stats - extracted methods

--- src/main/java/TextStatsServlet.java
+++ src/main/java/TextStatsServlet.java
@@ -14,26 +14,39 @@ public class TextStatsServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
 
-        writer.println("<p>" + text + "</p>");
-        writer.println("<h1>Ilość znaków: " + text.length() + "</h1>");
+        countAllChars(text, writer);
+        countCharsWithoutSpaces(text, writer);
+        countWords(text, writer);
+        isPalindrome(text, writer);
+    }
 
-        int countCharsWithoutSpaces = 0;
-        for(int i = 0; i < text.length(); i++) {
-            if(text.charAt(i) != ' ')
-                countCharsWithoutSpaces++;
-        }
-        writer.println("<h1>Ilość znaków bez spacji: " + countCharsWithoutSpaces + "</h1>");
-
-        String[] split = text.split(" ");
-        int countWords = split.length;
-        writer.println("<h1>Ilość słów: " + countWords + "</h1>");
-
+    private void isPalindrome(String text, PrintWriter writer) {
         String reversed = "";
-        for(int i = text.length()-1; i >= 0; i--) {
-             reversed += text.charAt(i);
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
         }
         boolean isPalindrome = reversed.equals(text);
         writer.println("<h1>Odwrócony text: " + reversed + "</h1>");
         writer.println("<h1>Palindrom: " + isPalindrome + "</h1>");
+    }
+
+    private void countWords(String text, PrintWriter writer) {
+        String[] split = text.split(" ");
+        int countWords = split.length;
+        writer.println("<h1>Ilość słów: " + countWords + "</h1>");
+    }
+
+    private void countCharsWithoutSpaces(String text, PrintWriter writer) {
+        int charsWithoutSpaces = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) != ' ')
+                charsWithoutSpaces++;
+        }
+        writer.println("<h1>Ilość znaków bez spacji: " + charsWithoutSpaces + "</h1>");
+    }
+
+    private void countAllChars(String text, PrintWriter writer) {
+        writer.println("<i>" + text + "</i>");
+        writer.println("<h1>Ilość znaków: " + text.length() + "</h1>");
     }
 }
---
+++ src/main/java/TextStatsUtils.java
@@ -0,0 +1,5 @@
+import java.io.PrintWriter;
+
+public class TextStatsUtils {
+
+}
